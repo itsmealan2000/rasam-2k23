55c39a44b57967ca681291b97e5ff0344919e411
Shine Santhosh <shinesanthosh20000@gmail.com>
2023-04-03 13:46:15 +0530

    Update events route to use dynamic slugs
    - Remove the permanent slug in CMS query added for testing
    - Modify the CMS query to use the slug from the props

--- src/pages/events/[event].js
+++ src/pages/events/[event].js
@@ -20,7 +20,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   const url = process.env.CMSURL
-  const query = `*[_type == 'events' && slug.current == 'test-event']{name, short, date, details,cords[]{name, contact, email},image{asset->{url}}}`
+  const query = `*[_type == 'events' && slug.current == ${params.event}]{name, short, date, details,cords[]{name, contact, email},image{asset->{url}}}`
   const res = await fetch(url + encodeURIComponent(query))
   const data = await res.json()
 
