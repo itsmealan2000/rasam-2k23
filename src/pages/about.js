e8872f8088e7411a578a9ec13b85a0c67f7b72ed
Shine Santhosh <shinesanthosh20000@gmail.com>
2023-04-11 11:21:17 +0530

    Add data from CMS to about page Fix #23
    Fetch static data fro about page from CMS

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,22 @@ import NavBar from '../components/navbar'
 import SEO from '../components/SEO'
 import styles from '../styles/About.module.scss'
 
-const About = () => {
+export const getStaticProps = async () => {
+  const url = process.env.CMSURL
+  const query = `*[_type == 'pages' && slug.current == 'pages']{about{ title, Content[]{children[]{_type, text}} ,metadata {title, desc} } }`
+  const res = await fetch(url + encodeURIComponent(query))
+  const data = await res.json()
+
+  return {
+    props: { data: data.result },
+  }
+}
+
+const About = ({ data }) => {
+  const elemList = []
+  const pagedata = data[0].about
+  const [bodyContent, useBodyContent] = useState([])
+
   const videoRef = useRef(null)
 
   const [isMobile, setIsMobile] = useState(false)
@@ -36,16 +51,21 @@ const About = () => {
       video.load()
     }
   }, [isMobile])
+
+  const children = pagedata.Content[0].children
+
+  for (let i in children) {
+    console.log(children[i]._type, children[i].text)
+    if (children[i]._type == 'span') {
+      elemList.push(<span>{children[i].text}</span>)
+    }
+  }
+
   return (
     <div>
       <SEO title='About' description='This is all about the about page' />
       <NavBar />
-      <div className={styles.videoContainer}>
-        <video autoPlay muted loop ref={videoRef}>
-          <source src='/videos/raw_background.mp4' type='video/mp4' />
-        </video>
-        This is the about page Yaaya
-      </div>
+      {elemList}
     </div>
   )
 }
