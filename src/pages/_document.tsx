8b2808cdf1ad4d08e32d97130dd65c1c5287620c
Jayasurya <jayasooryapj@gmail.com>
2023-04-13 00:01:44 +0530

    about page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel='icon' href='/images/logo.png'/>
+        <link rel="icon" href="/images/logo.png" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
