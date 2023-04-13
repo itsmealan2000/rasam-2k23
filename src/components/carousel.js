e20e46497a64eff6a6cb27db2c87883d2f65ab58
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-04-13 16:12:49 +0530

    Add carousel image src from CMS
    - The images inside the about page carousel is fetched from CMS

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,53 +1,18 @@
-import { Carousel } from "react-bootstrap";
-import styles from "../styles/Carousel.module.scss";
+import { Carousel } from 'react-bootstrap'
+import styles from '../styles/Carousel.module.scss'
 
-const ImgCarousel = () => {
-  return (
-    <Carousel className={styles.bsCarousel}>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/1.jpg"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/2.jpg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/3.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/4.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/5.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="/images/carousel/6.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-  );
-};
+const ImgCarousel = ({ cardata }) => {
+  let itemList = []
 
-export default ImgCarousel;
+  for (let i in cardata) {
+    itemList.push(
+      <Carousel.Item interval={1000} key={i}>
+        <img className='d-block w-100' src={cardata[i].asset.url} alt='slide' />
+      </Carousel.Item>
+    )
+  }
+
+  return <Carousel className={styles.bsCarousel}>{itemList}</Carousel>
+}
+
+export default ImgCarousel
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,73 +1,74 @@
-import React, { useEffect, useState, useRef } from "react";
-import NavBar from "../components/navbar";
-import SEO from "../components/SEO";
-import styles from "../styles/About.module.scss";
-import ImgCarousel from "../components/carousel";
+import React, { useEffect, useState, useRef } from 'react'
+import NavBar from '../components/navbar'
+import SEO from '../components/SEO'
+import styles from '../styles/About.module.scss'
+import ImgCarousel from '../components/carousel'
 
 export const getStaticProps = async () => {
-  const url = process.env.CMSURL;
-  const query = `*[_type == 'pages' && slug.current == 'pages']{about{ title, Content[]{children[]} ,metadata {title, desc} } }`;
-  const res = await fetch(url + encodeURIComponent(query));
-  const data = await res.json();
+  const url = process.env.CMSURL
+  const query = `*[_type == 'pages' && slug.current == 'pages']{about{ title, Content[]{children[]} , carousel[]{ asset->{url} },metadata {title, desc, image{ asset->{url} }} } }`
+  const res = await fetch(url + encodeURIComponent(query))
+  const data = await res.json()
 
   return {
     props: { data: data.result },
-  };
-};
+  }
+}
 
 const About = ({ data }) => {
-  const elemList = [];
-  const pagedata = data[0].about;
-  const [bodyContent, useBodyContent] = useState([]);
+  
+  const elemList = []
+  const pagedata = data[0].about
+  const [bodyContent, useBodyContent] = useState([])
 
-  const videoRef = useRef(null);
+  const videoRef = useRef(null)
 
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const mobileQuery = window.matchMedia("(max-width: 768px)");
-    setIsMobile(mobileQuery.matches);
+    const mobileQuery = window.matchMedia('(max-width: 768px)')
+    setIsMobile(mobileQuery.matches)
 
     const handleResize = () => {
-      setIsMobile(mobileQuery.matches);
-    };
-    mobileQuery.addEventListener("change", handleResize);
+      setIsMobile(mobileQuery.matches)
+    }
+    mobileQuery.addEventListener('change', handleResize)
 
     return () => {
-      mobileQuery.removeEventListener("change", handleResize);
-    };
-  }, []);
+      mobileQuery.removeEventListener('change', handleResize)
+    }
+  }, [])
 
   useEffect(() => {
     if (videoRef.current) {
-      const video = videoRef.current;
-      const source = video.querySelector("source");
+      const video = videoRef.current
+      const source = video.querySelector('source')
 
       if (isMobile) {
-        source.src = "/videos/mob_background.mp4";
+        source.src = '/videos/mob_background.mp4'
       } else {
-        source.src = "/videos/raw_background.mp4";
+        source.src = '/videos/raw_background.mp4'
       }
 
-      video.load();
+      video.load()
     }
-  }, [isMobile]);
+  }, [isMobile])
 
-  const children = pagedata.Content[0].children;
+  const children = pagedata.Content[0].children
 
   for (let i in children) {
-    console.log(children[i]._type, children[i].text);
-    if (children[i].marks[0] == "strong")
-      elemList.push(<strong>{children[i].text}</strong>);
-    else if (children[i]._type == "span")
-      elemList.push(<span>{children[i].text}</span>);
+    
+    if (children[i].marks[0] == 'strong')
+      elemList.push(<strong>{children[i].text}</strong>)
+    else if (children[i]._type == 'span')
+      elemList.push(<span>{children[i].text}</span>)
   }
 
   return (
     <main className={styles.aboutMain}>
-      <SEO title="About" description="This is all about the about page" />
+      <SEO title='About' description='This is all about the about page' />
       <NavBar />
-      <div className={styles.aboutMask} id="mask">
+      <div className={styles.aboutMask} id='mask'>
         <div className={styles.aboutText}>
           <h1>
             ABOUT <span>RASAM</span>
@@ -77,7 +78,7 @@ const About = ({ data }) => {
         </div>
         <h1 className={styles.cHeader}>GLIMPSES OF RASAM</h1>
         <div className={styles.carouselContainer}>
-          <ImgCarousel />
+          <ImgCarousel cardata={pagedata.carousel} />
         </div>
         <h1 className={styles.cHeader}>SPONSORS</h1>
         <div className={styles.sponsorContainer}>
@@ -85,7 +86,7 @@ const About = ({ data }) => {
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default About;
+export default About
